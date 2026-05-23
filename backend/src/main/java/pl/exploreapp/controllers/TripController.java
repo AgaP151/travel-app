8b7c41c0dbcc4f904294304659073d1d2286cd4a
Agnieszka Piotrowska <agnieszka.piotrowska@studenci.collegiumwitelona.pl>
2026-05-23 12:01:26 +0200

    first step -test

---
+++ backend/src/main/java/pl/exploreapp/controllers/TripController.java
@@ -0,0 +1,23 @@
+package pl.exploreapp.backend.controllers;
+
+import pl.exploreapp.backend.models.Trip;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/trips")
+public class TripController {
+
+    @GetMapping
+    public List<Trip> getAllTrips() {
+        // Na razie zwracamy testowe dane wpisane na sztywno, żeby sprawdzić czy działa
+        Trip trip1 = new Trip("Wycieczka do Paryża", "Cudowny weekend w stolicy Francji", 2499.0);
+        Trip trip2 = new Trip("Słoneczna Barcelona", "Relaks na plaży i zwiedzanie dzieł Gaudiego", 1999.0);
+        
+        return Arrays.asList(trip1, trip2);
+    }
+}

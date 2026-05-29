ac35a884a35b210400e4e98b083a36abaab59290
Agnieszka Piotrowska <agnieszka.piotrowska@studenci.collegiumwitelona.pl>
2026-05-29 21:59:37 +0200

    Add auth pages routing

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,10 @@
+function LoginPage() {
+  return (
+    <div>
+      <h1>Logowanie</h1>
+      <p>Travel planner app</p>
+    </div>
+  );
+}
+
+export default LoginPage;
---
+++ frontend/src/pages/RegisterPage.jsx
@@ -0,0 +1,10 @@
+function RegisterPage() {
+  return (
+    <div>
+      <h1>Rejestracja</h1>
+      <p>Utwórz konto</p>
+    </div>
+  );
+}
+
+export default RegisterPage;

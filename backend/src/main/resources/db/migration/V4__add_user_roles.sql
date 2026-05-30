c7092da5163af3b72999320263325042cd8085ac
E-Kmiecik <elzbieta.kmiecik@studenci.collegiumwitelona.pl>
2026-05-30 14:54:37 +0200

    Poprawa hasla admina na bash BCrypt po Code Rewiew

--- backend/src/main/resources/db/migration/V4__add_user_roles.sql
+++ backend/src/main/resources/db/migration/V4__add_user_roles.sql
@@ -2,6 +2,6 @@
 -- Domyślnie każdy nowy użytkownik będzie zwykłym 'ROLE_USER'.
 ALTER TABLE users ADD COLUMN role VARCHAR(50) DEFAULT 'ROLE_USER';
 
--- 2. Dodajemy testowego administratora do bazy, żeby Aga miała na kim testować uprawnienia
+-- 2. Dodajemy testowego administratora do bazy - testowanie uprawnień
 INSERT INTO users (name, email, password_hash, role) VALUES 
-('Szef Wszystkich Szefów', 'admin@example.com', 'haslo_admina_123', 'ROLE_ADMIN');
+('Szef Wszystkich Szefów', 'admin@example.com', '$2a$10$JL2pw1OYKj4lDtUTQUry9umEhsgs9Ro17lBjDsgjofg0ydmxc5g8y', 'ROLE_ADMIN');
